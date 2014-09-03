13382e0f95f9d9a795aeb0a6b0829aeea25c87bb
Julian Amaya <julian@monoku.com>
2014-09-03 00:24:06 -0500

    ajustes, agrego sponsors al admin

--- landing/admin.py
+++ landing/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Speaker
+from .models import Speaker, Sponsor
 
 admin.site.register(Speaker)
+admin.site.register(Sponsor)
--- landing/views.py
+++ landing/views.py
@@ -2,7 +2,7 @@ from datetime import date
 
 from django.shortcuts import render
 
-from .models import Speaker
+from .models import Speaker, Sponsor
 
 FECHA_CIERRE = date(2014, 11, 7)
 
@@ -10,5 +10,6 @@ FECHA_CIERRE = date(2014, 11, 7)
 def home(request):
     data = {}
     data['speakers'] = Speaker.objects.all()
+    data['sponsors'] = Sponsor.objects.all()
     data['days_left'] = max((FECHA_CIERRE - date.today()).days, 0)
     return render(request, 'index.html', data)
