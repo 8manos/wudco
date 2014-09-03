d23a6344525555f3b3e745da5f79877cdefa75ea
Julian Amaya <julian@monoku.com>
2014-09-02 22:47:25 -0500

    ajstes para S3

--- wudco/settings.py
+++ wudco/settings.py
@@ -105,5 +105,5 @@ ALLOWED_HOSTS = ['*']
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET')
