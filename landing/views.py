857abcdb13ceebb243fd7e37f6bdf6957eb55b08
Julian Amaya <julian@monoku.com>
2014-09-05 08:07:12 -0500

    incluyo charlas en la vista

--- landing/views.py
+++ landing/views.py
@@ -2,7 +2,7 @@ from datetime import date
 
 from django.shortcuts import render
 
-from .models import Speaker, Sponsor
+from .models import Speaker, Sponsor, Talk
 
 FECHA_CIERRE = date(2014, 11, 7)
 
@@ -11,5 +11,6 @@ def home(request):
     data = {}
     data['speakers'] = Speaker.objects.all()
     data['sponsors'] = Sponsor.objects.all()
+    data['talks'] = Talk.objects.all()
     data['days_left'] = max((FECHA_CIERRE - date.today()).days, 0)
     return render(request, 'index.html', data)
