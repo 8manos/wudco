0fe8d182eaac5b5bb392a5ac4785365b33957b1d
Carlos Sosa <cesosag@gmail.com>
2014-09-09 12:13:18 -0500

    gulpfile for sass watching only

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,26 @@
-var sass = require('gulp-ruby-sass');
 var gulp = require('gulp');
-var browserSync = require();
+var sass = require('gulp-ruby-sass');
+//var browserSync = require('browser-sync');
+//var reload = browserSync.reload;
+
+gulp.task('sass', function(){
+	return gulp.src('content/css/main.sass')
+		.pipe(sass({
+			sourcemap: true
+		}))
+		.on('error', function(err){
+			console.log(err.message);
+		})
+		.pipe(gulp.dest('content/css'));
+		//.pipe(reload({stream:true}));
+});
 
 gulp.task('default', function(){
-	
+	/*browserSync({
+		server: {
+			baseDir: 'content'
+		}
+	});*/
+
+	gulp.watch('content/css/*.sass', ['sass']);
 });
