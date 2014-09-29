e3558e5e699d7ffa943c4dbde881a306fb2487bb
Carlos Sosa <cesosag@gmail.com>
2014-09-29 15:48:20 -0500

    Fixed menu and active state for li in mobile menu

--- content/js/main.js
+++ content/js/main.js
@@ -7,4 +7,27 @@ $(document).on('ready', function(){
 			$('.menu').removeClass('is-visible');
 		}
 	});
+
+	var scrollTimeout;
+
+	$(window).scroll(function () {
+		if (scrollTimeout) {
+			clearTimeout(scrollTimeout);
+			scrollTimeout = null;
+		}
+		scrollTimeout = setTimeout(fixNav, 100);
+	});
+
+	fixNav = function(){
+		var ScrollTop = $(window).scrollTop();
+		if(ScrollTop >= $('.nav-container').offset().top){
+			$('nav').addClass('is-fixed');
+			var nav_height = $('nav').innerHeight();
+			$('header').css('margin-bottom', nav_height);
+		}
+		else{
+			$('nav').removeClass('is-fixed');
+			$('header').css('margin-bottom', 0);
+		}
+	};
 });
