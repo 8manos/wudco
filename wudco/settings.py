8cbda207e4167c797e56e1a3944820b6fc2799c3
Julian Amaya <julian@monoku.com>
2014-09-29 15:51:40 -0500

    ajuste de settings, no entiendo pq se deshabilito s3

--- wudco/settings.py
+++ wudco/settings.py
@@ -108,7 +108,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 if [(i, k) for i, k in os.environ.items() if 'heroku' in k]:
-    #DEBUG = False
+    DEBUG = False
     import dj_database_url
     DATABASES['default'] = dj_database_url.config()
 
@@ -118,8 +118,8 @@ if [(i, k) for i, k in os.environ.items() if 'heroku' in k]:
     # Allow all host headers
     ALLOWED_HOSTS = ['*']
 
-    """DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET')"""
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET')
